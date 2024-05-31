5ab3fa4bcb43da2dc39304ac6aaa087a5121d8ea
noorpiash <noorpiash1234@gmail.com>
2024-05-31 10:17:02 +0600

    task add

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const toggleScreen = () => {
 menuBtn.addEventListener("click", toggleScreen);
 backBtn.addEventListener("click", toggleScreen);
 
+
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addTaskForm = document.querySelector(".add-task");
+const blackBackdrop = document.querySelector(".black-backdrop");
+
+
+const toggleAddTaskForm = () => {
+    addTaskForm.classList.toggle("active");
+    blackBackdrop.classList.toggle("active");
+    addTaskBtn.classList.toggle("active");
+
+};
+
+addTaskBtn.addEventListener("click", toggleAddTaskForm);
+blackBackdrop.addEventListener("click", toggleAddTaskForm);
